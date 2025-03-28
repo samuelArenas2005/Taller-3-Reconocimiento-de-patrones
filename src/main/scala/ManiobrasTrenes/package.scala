969038626ae85d1cfbd7cd542da370da314dd9f9
Ckancord33 <niccord33@gmail.com>
2025-03-28 14:15:05 -0500

    Todas las funciones implementadas

--- src/main/scala/ManiobrasTrenes/package.scala
+++ src/main/scala/ManiobrasTrenes/package.scala
@@ -11,28 +11,39 @@ package object ManiobrasTrenes {
 
   def aplicarMovimiento(e:Estado, m:Movimiento): Estado = {
     m match {
-    case Uno(n)  => n match
-      case x if x > 0 =>  (e._1 take(e._1.length - n),e._1.takeRight(n) ++ e._2 ,e._3)
-      case x if x < 0  => (e._1 ++ e._2.take(-n),e._2.drop(-n),e._3)
-      case _ => e
-    case Dos(n) => n match
-      case x if x > 0 => (e._1 take(e._1.length - n),e._2,e._1.takeRight(n) ++ e._3)
-      case x if x < 0  => (e._1 ++ e._3.take(-n),e._2,e._3.drop(-n))
-      case _ => e
+      case Uno(n)  =>
+        if (n > 0) (e._1.take(e._1.length - n), e._1.drop(e._1.length - n) ++ e._2, e._3)
+        else if (n < 0) (e._1 ++ e._2.take(-n), e._2.drop(-n), e._3)
+        else e
+
+      case Dos(n) =>
+        if (n > 0) (e._1.take(e._1.length - n), e._2, e._1.drop(e._1.length - n) ++ e._3)
+        else if (n < 0) (e._1 ++ e._3.take(-n), e._2, e._3.drop(-n))
+        else e
     }
   }
 
-  def aplicarMovimientos(e:Estado, movs:Maniobra) : List[Estado] = {
-    def definirEstados(e:Estado, movs:Movimiento, x:List[Estado]) : List[Estado] = {
-      List(e)
-    }
-    definirEstados(e,movs.head,List(e))
+  def aplicarMovimientos(e: Estado, movs: Maniobra): List[Estado] = movs match{
+    case y :: Nil => aplicarMovimiento(e, y) :: Nil
+    case y :: ys => e :: aplicarMovimientos(aplicarMovimiento(e, y), ys)
   }
 
   def definirManiobra(t1:Tren,t2:Tren): Maniobra = {
-    val b: Uno = Uno(1)
-    val a = List(b)
-    a
+
+    def definirMovimiento(e: Estado, movs: Maniobra, t2: Tren): Maniobra = e match{
+      case (Nil, Nil, Nil) => movs
+      case (x:: xs,Nil,Nil) => {
+        if ( x == t2.head ) definirMovimiento((xs,Nil,Nil),movs, t2.tail)
+        else{
+          val mover = e._1 dropWhile (_ != t2.head)
+          definirMovimiento(aplicarMovimiento(e, Uno(mover.length)), Uno(mover.length) :: movs, t2)
+        }
+      }
+      case(y :: ys, x :: xs, Nil) => definirMovimiento(aplicarMovimiento(e, Dos(e._1.length)), Dos(e._1.length) :: movs, t2 )
+      case(Nil, x :: xs, y :: ys) => definirMovimiento(aplicarMovimiento(e, Uno(-e._2.length)), Uno(-e._2.length) :: movs, t2 )
+      case(x :: xs, Nil, y :: ys) => definirMovimiento(aplicarMovimiento(e, Dos(-e._3.length)), Dos(-e._3.length) :: movs, t2 )
+    }
+    definirMovimiento((t1, Nil, Nil), Nil, t2).reverse
   }
 
 }
